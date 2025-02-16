57ef4ddd01743a43c74c9528c0b615c2454cda18
MrMartin0625 <collins.sylvia1920@gmail.com>
2025-02-16 16:09:04 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// Urban Sustainability Solutions - Homepage
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>🌿 Welcome to Urban Sustainability Solutions 🌆</h1>
+      <p>Empowering communities for a greener, brighter future.</p>
+    </div>
+  );
+}
